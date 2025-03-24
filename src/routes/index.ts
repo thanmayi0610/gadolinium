09753968118fc17b6c792d6d96bea73190efb2ae
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-03-24 20:37:44 +0530

    Implement sign-up route with error handling in authentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,31 @@
+import { sign } from "crypto";
+import { signUpWithUsernameAndPassword } from "../controller/authentication";
+import { SignUpWithUsernameAndPasswordError } from "../controller/authentication/+type";
 import { Hono } from "hono";
 
 export const hono = new Hono();
+hono.post("/authentication/signup", async (context) => {
+    const{ username,password} = await context.req.json();
+    try {
+    const result= await signUpWithUsernameAndPassword({
+        username,
+        password});
+        return context.json({
+            data: result,
+        },
+        201);
+    }
+    catch (e) {
+        if (e === SignUpWithUsernameAndPasswordError.UNKNOWN) {
+            return context.json({
+                message: "Unknown error",
+            }, 500);
+        }
+
+    }
+
+
+});
 
 hono.get("/health", (context) => {
   return context.json(
