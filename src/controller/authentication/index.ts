1fbe74536d5a27fed0dbbcd133b99b1b1f0109ab
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-03-25 10:31:05 +0530

    Add login functionality and refactor signup process with helper functions

--- src/controller/authentication/index.ts
+++ src/controller/authentication/index.ts
@@ -1,26 +1,64 @@
 import { createHash } from "crypto";
 
-import { SignUpWithUsernameAndPasswordError, type SignUpWithUsernameAndPasswordResult } from "./+type";
+import {
+  LogInWtihUsernameAndPasswordError,
+  SignUpWithUsernameAndPasswordError,
+  type LogInWithUsernameAndPasswordResult,
+  type SignUpWithUsernameAndPasswordResult,
+} from "./+type";
 import { prismaClient } from "../../extras/prisma";
 import jwt from "jsonwebtoken";
 import { jwtSecretKey } from "../../../environment";
+
+const createJWToken = (parameters: { id: string; username: string }): string => {
+  // Generate token
+  const jwtPayload: jwt.JwtPayload = {
+    iss: "https://purpleshorts.co.in",
+    sub: parameters.id,
+    username: parameters.username,
+  };
+
+  const token = jwt.sign(jwtPayload, jwtSecretKey, {
+    expiresIn: "30d",
+  });
+
+  return token;
+};
+
+export const checkIfUserExistsAlready = async (parameters: { username: string }): Promise<boolean> => {
+  const existingUser = await prismaClient.user.findUnique({
+    where: {
+      username: parameters.username,
+    },
+  });
+
+  if (existingUser) {
+    return true;
+  }
+
+  return false;
+};
+
+export const createPasswordHash = (parameters: { password: string }): string => {
+  return createHash("sha256").update(parameters.password).digest("hex");
+};
 
 export const signUpWithUsernameAndPassword = async (parameters: {
   username: string;
   password: string;
 }): Promise<SignUpWithUsernameAndPasswordResult> => {
   try {
-    const existingUser = await prismaClient.user.findUnique({
-      where: {
-        username: parameters.username,
-      },
+    const isUserExistingAlready = await checkIfUserExistsAlready({
+      username: parameters.username,
     });
 
-    if (existingUser) {
+    if (isUserExistingAlready) {
       throw SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME;
     }
 
-    const passwordHash = createHash("sha256").update(parameters.password).digest("hex");
+    const passwordHash = createPasswordHash({
+      password: parameters.password,
+    });
 
     const user = await prismaClient.user.create({
       data: {
@@ -29,15 +67,9 @@ export const signUpWithUsernameAndPassword = async (parameters: {
       },
     });
 
-    // Generate token
-    const jwtPayload: jwt.JwtPayload = {
-      iss: "https://purpleshorts.co.in",
-      sub: user.id,
+    const token = createJWToken({
+      id: user.id,
       username: user.username,
-    };
-
-    const token = jwt.sign(jwtPayload, jwtSecretKey, {
-      expiresIn: "30d",
     });
 
     const result: SignUpWithUsernameAndPasswordResult = {
@@ -49,5 +81,44 @@ export const signUpWithUsernameAndPassword = async (parameters: {
   } catch (e) {
     console.log("Error", e);
     throw SignUpWithUsernameAndPasswordError.UNKNOWN;
+  }
+};
+
+export const logInWithUsernameAndPassword = async (parameters: {
+  username: string;
+  password: string;
+}): Promise<LogInWithUsernameAndPasswordResult> => {
+  try {
+    // 1. Create the password hash
+    const passwordHash = createPasswordHash({
+      password: parameters.password,
+    });
+
+    // 2. Find the user with the username and password hash
+    const user = await prismaClient.user.findUnique({
+      where: {
+        username: parameters.username,
+        password: passwordHash,
+      },
+    });
+
+    // 3. If the user is not found, throw an error
+    if (!user) {
+      throw LogInWtihUsernameAndPasswordError.INCORRECT_USERNAME_OR_PASSWORD;
+    }
+
+    // 4. If the user is found, create a JWT token and return it
+    const token = createJWToken({
+      id: user.id,
+      username: user.username,
+    });
+
+    return {
+      token,
+      user,
+    };
+  } catch (e) {
+    console.log("Error", e);
+    throw LogInWtihUsernameAndPasswordError.UNKNOWN;
   }
 };
