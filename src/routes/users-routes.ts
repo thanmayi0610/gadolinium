92c21e4e24bfe69c556ab8326a0a7ba2b726794f
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-03-25 15:26:39 +0530

    Refactor user routes to improve token handling and add user retrieval by ID

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,23 +1,48 @@
-import {Hono } from "hono";
-import jwt from "jsonwebtoken";
-import { jwtSecretKey } from "../../environment";
+import { Hono } from "hono";
 import { prismaClient } from "../extras/prisma";
+import { tokenMiddleware } from "./middlewares/token-middleware";
 
-export const userRoutes = new Hono();
+export const usersRoutes = new Hono();
 
-userRoutes.get("/", async (context, next) => {
-    const token = context.req.header("token");
-    if (!token) {
-      return context.json({ error: "Unauthorized" }, 401);
-    }  try {
-      const verified = jwt.verify(token, jwtSecretKey);
-      if (!verified) {
-        return context.json({ error: "Invalid Token" }, 401);
-      }
-      await next();
-    } catch (err) {
-      return context.json({ error: "Invalid Token" }, 401);
-    }}, async (c) => {
-    const users = await prismaClient.user.findMany();
-    return c.json(users, 200);
+usersRoutes.get("/me", tokenMiddleware, async (context) => {
+  const userId = context.get("userId");
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: userId,
+    },
   });
+
+  if (user) {
+    return context.json(user, 200);
+  } else {
+    return context.json(
+      {
+        message: "Missing /me User",
+      },
+      404
+    );
+  }
+});
+
+usersRoutes.get("", tokenMiddleware, async (context) => {
+  const users = await prismaClient.user.findMany();
+
+  return context.json(users, 200);
+});
+// userRoutes.get("/", async (context, next) => {
+//     const token = context.req.header("token");
+//     if (!token) {
+//       return context.json({ error: "Unauthorized" }, 401);
+//     }  try {
+//       const verified = jwt.verify(token, jwtSecretKey);
+//       if (!verified) {
+//         return context.json({ error: "Invalid Token" }, 401);
+//       }
+//       await next();
+//     } catch (err) {
+//       return context.json({ error: "Invalid Token" }, 401);
+//     }}, async (c) => {
+//     const users = await prismaClient.user.findMany();
+//     return c.json(users, 200);
+//   });
